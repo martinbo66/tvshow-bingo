e00a1c9d1c37f060a27a8010e9ed4b19cd7283fd
Bo Martin <martinbo66@gmail.com>
2022-06-19 20:07:23 -0400

    Remove path which was ignored anyway.

--- src/main/java/org/bomartin/resource/TvShowResource.java
+++ src/main/java/org/bomartin/resource/TvShowResource.java
@@ -4,9 +4,7 @@ import io.quarkus.hibernate.orm.rest.data.panache.PanacheRepositoryResource;
 import org.bomartin.model.TvShow;
 import org.bomartin.repository.TvShowRepository;
 
-import javax.ws.rs.Path;
 import java.util.UUID;
 
-@Path("/tvshows")
 public interface TvShowResource extends PanacheRepositoryResource<TvShowRepository, TvShow, UUID> {
 }
