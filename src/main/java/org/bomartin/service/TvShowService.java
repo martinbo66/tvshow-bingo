8e5e6c6fb6f4b6dae9818a716af5403384daffa3
Bo Martin <martinbo66@gmail.com>
2022-09-18 20:01:35 -0400

    Constructor in the unused TVShowService

--- src/main/java/org/bomartin/service/TvShowService.java
+++ src/main/java/org/bomartin/service/TvShowService.java
@@ -2,7 +2,13 @@ package org.bomartin.service;
 
 import org.bomartin.model.TvShow;
 
+import java.util.UUID;
+
 public class TvShowService {
+
+    public TvShow getTvShow(UUID id) {
+        return new TvShow();
+    }
 
     public TvShow deletePhrase() {
         return new TvShow();
